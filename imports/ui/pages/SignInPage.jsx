3a0f92f0c2b785ddf9580bdcf220d45be6544b1d
Israel Lugo <hostelixisrael@gmail.com>
2017-11-30 18:11:12 -0400

    [MOD] add procedure to login and signup

--- imports/ui/pages/SignInPage.jsx
+++ imports/ui/pages/SignInPage.jsx
@@ -1,9 +1,57 @@
 import React, {Component} from 'react';
+import {Meteor} from 'meteor/meteor';
+import TextField from '@atlaskit/field-text';
+import Btn from '@atlaskit/button';
 
 class SignInPage extends Component {
+
+    constructor(props) {
+        super(...props);
+
+        this.state = {
+            form: {
+                email: '',
+                password: ''
+            }
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChangeField(nameField) {
+        let form = {...this.state.form};
+        return (e) => {
+            form[nameField] = e.target.value;
+            this.setState({form});
+        }
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        let {email, password} = this.state.form;
+
+        Meteor.loginWithPassword(email, password, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                this.props.history.push('/home');
+            }
+        });
+    }
+
     render() {
         return (
-            <div>Codeando Meteor - SignInPage</div>
+            <div>
+                <form>
+                    <TextField label="Username or Email" value={this.state.form.email}
+                               onChange={this.handleChangeField("email")}/>
+                    <TextField label="Password" type="password" value={this.state.form.password}
+                               onChange={this.handleChangeField("password")}/>
+                    <br/>
+                    <Btn appearance="primary" onClick={this.handleSubmit}>Sign In</Btn>
+                </form>
+            </div>
         );
     }
 }
--- imports/ui/pages/SignUpPage.jsx
+++ imports/ui/pages/SignUpPage.jsx
@@ -1,9 +1,62 @@
 import React, {Component} from 'react';
+import TextField from '@atlaskit/field-text';
+import Btn from '@atlaskit/button';
+import {Accounts} from 'meteor/accounts-base';
 
 class SignUpPage extends Component {
+
+    constructor(props) {
+        super(...props);
+
+        this.state = {
+            form: {
+                username: '',
+                email: '',
+                password: ''
+            }
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChangeField(nameField) {
+        let form = {...this.state.form};
+        return (e) => {
+            form[nameField] = e.target.value;
+            this.setState({form});
+        }
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        let {username, email, password} = this.state.form;
+
+        Accounts.createUser({email: email, username: username, password: password}, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                this.props.history.push('/signin');
+            }
+        });
+    }
+
     render() {
         return (
-            <div>Codeando Meteor - SignUpPage</div>
+            <div>
+                <form>
+                    <TextField label="Username" value={this.state.form.username}
+                               onChange={this.handleChangeField("username")}/>
+
+                    <TextField label="Email" value={this.state.form.email}
+                               onChange={this.handleChangeField("email")}/>
+
+                    <TextField label="Password" type="password" value={this.state.form.password}
+                               onChange={this.handleChangeField("password")}/>
+                    <br/>
+                    <Btn appearance="primary" onClick={this.handleSubmit}>Sign In</Btn>
+                </form>
+            </div>
         );
     }
 }
