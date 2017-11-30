27b99483060d14fc2fa9517a134f22f1361ab2ab
Israel Lugo <hostelixisrael@gmail.com>
2017-11-30 17:11:08 -0400

    [ADD] add pages to app

---
+++ imports/ui/pages/SignInPage.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class SignInPage extends Component {
+    render() {
+        return (
+            <div>Codeando Meteor - SignInPage</div>
+        );
+    }
+}
+
+export default SignInPage;
---
+++ imports/ui/pages/SignUpPage.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class SignUpPage extends Component {
+    render() {
+        return (
+            <div>Codeando Meteor - SignUpPage</div>
+        );
+    }
+}
+
+export default SignUpPage;
