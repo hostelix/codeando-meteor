ac0834763161a076f548bff96f70bb3a891a79a8
Israel Lugo <hostelixisrael@gmail.com>
2017-12-06 16:58:12 -0400

    [MOD] change path pages

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import {Router, Switch, Route} from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 
-import SignInPage from '../../ui/pages/SignInPage';
-import SignUpPage from '../../ui/pages/SignUpPage';
-import NotFoundPage from '../../ui/pages/NotFoundPage';
+import SignInPage from '../../ui/pages/Auth/SignInPage';
+import SignUpPage from '../../ui/pages/Auth/SignUpPage';
+import NotFoundPage from '../../ui/pages/HttpError/NotFoundPage';
+import MainPage from '../../ui/pages/App/MainPage';
 
 const browserHistory = createBrowserHistory();
 
@@ -12,9 +13,9 @@ export const AppRoutes = () => (
     <div>
         <Router history={browserHistory}>
             <Switch>
-                <Route exact path="/" component={SignInPage}/>
-                <Route exact path="/signin" component={SignInPage}/>
-                <Route exact path="/signup" component={SignUpPage}/>
+                <Route exact path="/" component={MainPage}/>
+                <Route exact path="/login" component={SignInPage}/>
+                <Route exact path="/register" component={SignUpPage}/>
                 <Route path="*" component={NotFoundPage}/>
             </Switch>
         </Router>
